fa8b2df0ac204a16d2c42f593f30becbff9ed04e
Lucas Marques da Silva <lucas.msilva09@gmail.com>
2018-02-13 15:15:25 -0200

    Create the login and register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
 import { HomeModule } from './home/home.module';
 
 
@@ -22,6 +23,7 @@ const rootRouting: ModuleWithProviders = RouterModule.forRoot([], { useHash: tru
   ],
   imports: [
     BrowserModule,
+    AuthModule,
     SharedModule,
     HomeModule,
     rootRouting
